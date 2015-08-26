fb4ef6b268310228a0012449e322862f2867e350
sorinies <neoeye2@gmail.com>
2015-08-27 02:26:57 +0900

    add outDatedBrowser

---
+++ js/init.outdated.js
@@ -0,0 +1,23 @@
+//event listener: DOM ready
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function() {
+            if (oldonload) {
+                oldonload();
+            }
+            func();
+        }
+    }
+}
+//call plugin function after DOM ready
+addLoadEvent(function(){
+    outdatedBrowser({
+        bgColor: '#ed8123',
+        color: '#ffffff',
+        lowerThan: 'transform',
+        languagePath: '/_includes/outdate-kr.html'
+    })
+});
